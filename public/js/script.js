76f394d4f2d1227cbaaf2f966e8627f49b5516ba
Faizan <faizan1041@users.noreply.github.com>
2017-10-28 16:20:46 +0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -30,6 +30,7 @@ $("#question").on("keyup", function(event) {
                           '</li>';
         $('#dialogue').append(control);
         socket.emit('chat message', text);
+        $("#dialogue").animate({ scrollTop: $('#dialogue').prop("scrollHeight")}, 1000);
         $('#question').val('');
     }
 });
